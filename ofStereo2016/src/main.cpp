d3ad3994fdb8950e43182235f54713a19fced1f1
Johan Bichel Lindegaard <public@johan.cc>
2016-01-29 13:57:34 +0100

    two windows, gui app

--- ofStereo2016/src/main.cpp
+++ ofStereo2016/src/main.cpp
@@ -1,13 +1,26 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "GuiApp.hpp"
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
+    ofGLWindowSettings settings;
+    settings.width = 600;
+    settings.height = 600;
+    settings.setPosition(ofVec2f(300,0));
+    auto mainWindow = ofCreateWindow(settings);
+    
+    settings.width = 300;
+    settings.height = 300;
+    settings.setPosition(ofVec2f(0,0));
+    auto guiWindow = ofCreateWindow(settings);
+    
+    auto mainApp = make_shared<ofApp>();
+    auto guiApp = make_shared<GuiApp>();
+    
+    ofRunApp(guiWindow, guiApp);
+    ofRunApp(mainWindow, mainApp);
+    
+    ofRunMainLoop();
 
 }
