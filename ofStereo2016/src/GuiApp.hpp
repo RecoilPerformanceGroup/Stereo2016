396f5076396bbb53935dd1e04e701e3beff34138
Johan Bichel Lindegaard <public@johan.cc>
2016-01-29 13:57:44 +0100

    add guiapp

--- ofStereo2016/src/GuiApp.hpp
+++ ofStereo2016/src/GuiApp.hpp
@@ -1,5 +1,5 @@
 //
-//  uiApp.hpp
+//  GuiApp.hpp
 //  ofStereo2016
 //
 //  Created by Johan Bichel Lindegaard on 1/29/16.
@@ -9,8 +9,47 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxXmlSettings.h"
+#include "ofxOsc.h"
 
-class uiApp : public ofBaseApp{
+/**
+ 
+ This class should implement gui elemnts in a seperate gui window
+ an setup and osc listener to allow all gui elements to be controlled
+ 
+ osc listener should be able to 
+ 1. directly control parameter
+    parametername value
+ 
+ 2. trigger a fade from current value to target in time
+    parametername fadeto value milliseconds (optional easing function)
+ 
+ 3. trigger a fade from specific value to targetvalue
+    parametername fadefromto fromvalue targetvalue milliseconds (optional easing function)
+ 
+ 4. fade using specific easing functions optionally parsed as names
+    integrate
+ 
+ osc sender should broadcast all parameter changes
+ 
+ considerations 
+    -   how to implement normalisation?
+            Maybe an option to allow explicitly setting all parameters 
+            from a normalized value, automatically mapping to min / max in of
+            would also work for values already normalised
+            0-1 vs (-)1-1 normalisation
+    - how to format composite parameters, vec2f, vec3f, quarternion, grayscale, rgb, rgba color ...
+ 
+    - generic format for custom triggers
+    - group parameter presets - fade from preset to preset ?  / save / load
+    - groups of ui params that can be viewed and hidden together
+    - applying filters
+ 
+ **/
+
+
+class GuiApp : public ofBaseApp{
     
 public:
     void setup();
@@ -28,6 +67,13 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
+    
+    
+    ofxOscReceiver oscReceiver;
+    ofxOscSender oscSender;
+    
+    
+    
     
 };
 
