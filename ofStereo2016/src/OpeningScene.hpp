b626162fef7e387a7471e9aa492361f0c158ccbd
Stereo <stereo@macrecoill.local>
2016-04-11 13:42:29 +0200

    Begun OpeningScene

--- ofStereo2016/src/OpeningScene.hpp
+++ ofStereo2016/src/OpeningScene.hpp
@@ -1,8 +1,8 @@
 //
-//  SketchScene.hpp
+//  OpeningScene.hpp
 //  ofStereo2016
 //
-//  Created by Ole Kristensen on 10/03/2016.
+//  Created by Stereo on 11/04/16.
 //
 //
 
@@ -11,45 +11,50 @@
 #include "ofMain.h"
 #include "ofxStereoscopy.hpp"
 #include "VoroNode.hpp"
-#include "ofxGui.h"
 
-class SketchScene : public ofxStereoscopy::Scene {
-
+class OpeningScene : public ofxStereoscopy::Scene {
+    
 public:
-
+    
     ofParameter<float> speed {"speed", 0.01, 0.0, 10};
     ofParameter<float> radius {"radius", 20, 0.0, 800};
-    ofParameter<ofVec3f> origin {"origin", ofVec3f(0, 250, 400),ofVec3f(-1000, -1000, -1000),ofVec3f(1000,1000,1000)};
+    ofParameter<int> length {"length", 20, 0.0, 800};
+    ofParameter<float> height {"height", 100, 0.0, 300};
     ofParameter<ofVec3f> space {"space", ofVec3f(200, 200, 200),ofVec3f(0,0,0),ofVec3f(1000,1000,1000)};
+    ofParameter<ofFloatColor> color1 {"color1", ofFloatColor(1.0,1.0,1.0,1.0),ofFloatColor(0.0,0.0,0.0,0.0),ofFloatColor(1.0,1.0,1.0,1.0)};
+    ofParameter<ofFloatColor> color2 {"color2", ofFloatColor(1.0,1.0,1.0,1.0),ofFloatColor(0.0,0.0,0.0,0.0),ofFloatColor(1.0,1.0,1.0,1.0)};
     ofParameter<float> pivotRadius {"pivotRadius", 20, 0.0, 800};
     ofParameter<float> pivotSpeed {"pivotSpeed", 0.02, 0.0, 100.0};
     ofParameter<float> positionTowardsCamera {"positionTowardsCamera", 0, 0.0, 1.0};
     ofParameter<bool> reset {"reset", true};
-    //ofParameter<float> lineWidth {"lineWidth", 10.0, 0.0, 100.0};
+    ofParameter<bool> titleTrigger {"titleTrigger", false};
     ofParameter<ofVec2f> lineWidth {"lineWidth", ofVec2f(10.0,10.0), ofVec2f(-100,-100), ofVec2f(100,100)};
-
-    ofParameterGroup params {"sketch",
+    
+    ofParameterGroup params {"openingScene",
         enabled,
         qlab,
-        lineWidth,
         speed,
         radius,
-        origin,
+        length,
+        height,
+        color1,
+        color2,
         space,
         pivotRadius,
         pivotSpeed,
+        lineWidth,
         positionTowardsCamera,
-        reset
+        reset,
+        titleTrigger
     };
     
-    SketchScene() {
+    OpeningScene() {
         ofxStereoscopy::Scene::params = params;
     }
     
-    ofPath path;
-    ofVbo vbo;
-    ofFloatColor * colors;
-    vector<ofVec3f> points;
+    ofPath path[2];
+    ofVbo vbo[2];
+    deque<ofVec3f> points[2];
     ofVec3f noisePos;
     ofVec3f pivotNoisePos;
     ofVec3f spherePos;
@@ -57,11 +62,11 @@ public:
     void update();
     void setup();
     void onStageSize(ofVec3f& vec);
-    void resetLine();
-    void drawLine();
+    void resetLines();
+    void drawLines();
     
     ofShader wideLines;
-
+    
     OrganicMaterial mat;
     
     void drawModel();
--- ofStereo2016/src/scenes/PlateauScene.hpp
+++ ofStereo2016/src/scenes/PlateauScene.hpp
@@ -33,7 +33,7 @@ public:
     
     ofParameter<ofVec3f> plateauPivotOrigin {"plateauPivotOrigin", ofVec3f(0,0,0), ofVec3f(-1000,-10000,-1000), ofVec3f(1000,1000,1000) };
     ofParameter<float> plateauPivotNoiseAmount {"plateauPivotNoiseAmount", 0, 0, 1000 };
-    ofParameter<float> plateauPivotNoiseAmount {"plateauPivotNoiseAmount", 0, 0, 1000 };
+    ofParameter<float> plateauPivotNoiseSpeed {"plateauPivotNoiseSpeed", 0, 0, 2.0 };
     
     ofParameter<ofVec3f> selectPosition {"selectPosition", ofVec3f(0,-100000,-50000), ofVec3f(-10000,-10000,-100000), ofVec3f(10000,10000,100000) };
     ofParameter<float> selectRadius {"selectRadius", 2,0,2000};
@@ -45,6 +45,8 @@ public:
         plateauCellCount,
         plateauSize,
         plateauPivotOrigin,
+        plateauPivotNoiseAmount,
+        plateauPivotNoiseSpeed,
         plateauPosition,
         plateauRotation,
         plateauColor,
@@ -101,6 +103,8 @@ public:
     //OrganicMaterial matFloor;
     OrganicMaterial matAnaglyphRight;
     OrganicMaterial matAnaglyphLeft;
+    float noiseTime;
+    ofVec3f pivotNoise;
     
     ofMatrix4x4 pivotMatrix;
 
