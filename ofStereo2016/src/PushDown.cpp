710e4ed1cdc86ac6ba270333bc3a5b791b7b937a
Johan Bichel Lindegaard <public@johan.cc>
2016-04-13 14:59:06 +0200

    push down

--- ofStereo2016/src/PushDown.cpp
+++ ofStereo2016/src/PushDown.cpp
@@ -6,4 +6,91 @@
 //
 //
 
+
 #include "PushDown.hpp"
+
+void PushDown::setup() {
+    
+    seed.addListener(this, &PushDown::reconstruct<int>);
+    numCells.addListener(this, &PushDown::reconstruct<int>);
+    
+    reconstruct();
+    
+}
+
+void PushDown::draw() {
+    
+    
+    ofSetColor(255);
+    ofEnableAlphaBlending();
+    ofEnableLighting();
+    
+    mat.begin();
+    mainNode.draw(&mat);
+    mat.end();
+    
+}
+
+void PushDown::update() {
+    
+    if(doReconstruct){
+        reconstruct();
+        doReconstruct = false;
+    }
+    
+    mat.setDiffuseColor(clusterColor.get());
+    mat.setAmbientColor(clusterColor.get()*0.5);
+    mat.updateParameters();
+    box.setPosition(origin);
+    
+    
+    // push section down
+    
+    for( auto & n : mainNode.getChildren()) {
+        
+        ofVec3f pos = n->getGlobalPosition() * ofVec3f(1,0,1);
+        
+        // gradual push
+        n->renderPosOffset.y = ofxeasing::map_clamp(pushRadius-(pos.distance(dp()*ofVec3f(1,0,1))), 0, pushRadius, 0, pushDown, ofxeasing::linear::easeOut);
+        
+        
+        // binary push
+        //n->renderPosOffset.y = (pos.distance(dp() + pushOffset.get()) / pushRadius);
+        
+        /*
+        if(pos.distance(dp() + pushOffset.get()) < pushRadius) {
+            n->renderPosOffset.y = pushDown.get();
+        }*/
+        
+        
+    }
+    
+    
+}
+
+void PushDown::reconstruct(){
+    
+    ofVec3f _s = globalParams->getVec3f("stage_size_cm").get();
+    
+    box.setParent(world->origin);
+    box.set(_s.x, _s.x*0.5, _s.z, 4,2,4);
+    box.setScale(1,1,1);
+    box.setGlobalPosition( ofVec3f(0, -box.getHeight()/2, box.getDepth()/2) );
+    
+    origin.set(box.getGlobalPosition());
+    
+    ofSeedRandom(seed.get());
+    mainNode.setupFromBoundingBox(box.getWidth(), box.getHeight(), box.getDepth(), numCells, true,false,true);
+    mainNode.setParent(box);
+}
+
+void PushDown::drawModel() {
+    
+    ofSetColor(255,75);
+    box.drawWireframe();
+    ofSetColor(255,20);
+    mainNode.draw();
+    
+}
+
+
--- ofStereo2016/src/PushDown.hpp
+++ ofStereo2016/src/PushDown.hpp
@@ -6,9 +6,92 @@
 //
 //
 
-#ifndef PushDown_hpp
-#define PushDown_hpp
 
-#include <stdio.h>
+#pragma once
 
-#endif /* PushDown_hpp */
+#include "ofMain.h"
+#include "ofxStereoscopy.hpp"
+#include "VoroNode.hpp"
+#include "OrganicMaterial.hpp"
+#include "ofxEasing.h"
+
+class PushDown : public ofxStereoscopy::Scene {
+    
+public:
+    
+    // push down position = dancer position
+    
+    // select nearest 3
+    
+    // float push down amount
+    
+    ofParameter<float> pushDown {"push", 2, 0, -800};
+
+    ofParameter<float> pushRadius {"radius", 2, 0, 350};
+
+    ofParameter<ofVec3f> pushOffset {"origin", ofVec3f(0,0,0),
+        ofVec3f(-1000,-1000,-1000),
+        ofVec3f(1000,1000,1000)};
+    
+    
+    // sequentially
+    ofParameter<int> numCells {"cells", 2, 0, 200};
+    ofParameter<int> seed {"seed", 2, 0, 200};
+    
+    ofParameter<ofVec3f> origin {"origin", ofVec3f(0,0,0),
+        ofVec3f(-1000,-1000,-1000),
+        ofVec3f(1000,1000,1000)};
+    
+    ofParameter<ofFloatColor> clusterColor {"color", ofFloatColor(1,1,1,1), ofFloatColor(0,0,0,0), ofFloatColor(1,1,1,1)};
+    
+    ofParameterGroup voroNodeParams {"cluster",
+        numCells,
+        seed,
+        origin,
+        clusterColor,
+        pushDown,
+        pushRadius,
+        pushOffset,
+        
+    };
+    
+    ofParameterGroup params {"PushDown",
+        enabled,
+        qlab
+    };
+    
+    PushDown() {
+        voroNodeParams.add(mat.params);
+        params.add(voroNodeParams);
+        
+        ofxStereoscopy::Scene::params = params;
+    }
+    
+    void draw();
+    void drawModel();
+    void update();
+    void setup();
+    
+    template<typename type>
+    void reconstruct(type & t) {
+        doReconstruct = true;
+    }
+    void reconstruct();
+    bool doReconstruct = false;
+    
+    
+    ofBoxPrimitive box;
+    
+    VoroNode mainNode;
+    
+    //ofMesh intersect;
+    //ofMesh outMesh;
+    ofVec3f clusterRotation;
+    
+    OrganicMaterial mat;
+    
+    
+};
+
+
+
