fea87c89d94cd02d74cf3d8c4c87263f7ffe8831
Johan Bichel Lindegaard <public@johan.cc>
2016-04-14 12:13:32 +0200

    add box split

--- ofStereo2016/src/BoxSplit.hpp
+++ ofStereo2016/src/BoxSplit.hpp
@@ -6,9 +6,92 @@
 //
 //
 
-#ifndef BoxSplit_hpp
-#define BoxSplit_hpp
+#pragma once
 
-#include <stdio.h>
+#include "ofMain.h"
+#include "ofxStereoscopy.hpp"
+#include "VoroNode.hpp"
+#include "OrganicMaterial.hpp"
+#include "ofxEasing.h"
 
-#endif /* BoxSplit_hpp */
+class BoxSplit : public ofxStereoscopy::Scene {
+    
+public:
+    
+    // push down position = dancer position
+    
+    // select nearest 3
+    
+    // float push down amount
+    
+    
+    
+    ofParameter<ofVec3f> origin {"origin", ofVec3f(0,0,0),
+        ofVec3f(-1000,-1000,-1000),
+        ofVec3f(1000,1000,1000)};
+    
+    
+    ofParameter<float> split {"split", 0, 0, 400};
+    ofParameter<float> boxwidth {"width", 0, 0, 2};
+    
+    ofParameter<ofFloatColor> leftRightColor {"left right eye", ofFloatColor(1,1,1,1), ofFloatColor(0,0,0,0), ofFloatColor(1,1,1,1)};
+    
+    ofParameter<ofFloatColor> leftLeftColor {"left left eye", ofFloatColor(1,1,1,1), ofFloatColor(0,0,0,0), ofFloatColor(1,1,1,1)};
+    
+    ofParameter<ofFloatColor> rightRightColor {"right right eye", ofFloatColor(1,1,1,1), ofFloatColor(0,0,0,0), ofFloatColor(1,1,1,1)};
+    
+    ofParameter<ofFloatColor> rightLeftColor {"right left eye", ofFloatColor(1,1,1,1), ofFloatColor(0,0,0,0), ofFloatColor(1,1,1,1)};
+    
+    ofParameter<ofFloatColor> rightColor {"color", ofFloatColor(1,1,1,1), ofFloatColor(0,0,0,0), ofFloatColor(1,1,1,1)};
+    
+    ofParameterGroup boxp {"box",
+        split,
+        boxwidth,
+        leftLeftColor,
+        leftRightColor,
+        rightLeftColor,
+        rightRightColor
+    };
+    
+    ofParameterGroup params {"boxsplit",
+        enabled,
+        qlab
+    };
+    
+    BoxSplit() {
+        //voroNodeParams.add(mat.params);
+        params.add(boxp);
+        
+        ofxStereoscopy::Scene::params = params;
+    }
+    
+    void draw();
+    void drawModel();
+    void update();
+    void setup();
+    
+    ofNode floorCenter, wallCenter;
+    
+    ofBoxPrimitive boxLeftFloor;
+    ofBoxPrimitive boxRightFloor;
+    
+    ofBoxPrimitive boxLeftWall;
+    ofBoxPrimitive boxRightWall;
+    
+    
+    //ofMesh intersect;
+    //ofMesh outMesh;
+    ofVec3f clusterRotation;
+    
+    /*OrganicMaterial matRightLeft;
+    OrganicMaterial matRightRight;
+    
+    OrganicMaterial matLeftLeft;
+    OrganicMaterial matLeftRight;*/
+    
+    
+    
+};
+
+
+
